db33c262ef4bb4ce65ce814ef97c9b2297f45d8c
morshidulrahman <boydanger416@gmail.com>
2024-07-16 12:28:19 +0600

    register and login done

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
+const bcrypt = require("bcrypt");
+
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const cookieParser = require("cookie-parser");
@@ -9,6 +11,12 @@ const jwt = require("jsonwebtoken");
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.m73tovo.mongodb.net/?appName=Cluster0`;
 
@@ -21,6 +29,12 @@ const client = new MongoClient(uri, {
   },
 });
 
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production" ? true : false,
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+
 const dbConnect = async () => {
   try {
     client.connect();
@@ -30,6 +44,64 @@ const dbConnect = async () => {
   }
 };
 dbConnect();
+
+const Database = client.db("MFC");
+const appertmentsCollection = Database.collection("users");
+
+app.post("/users", async (req, res) => {
+  const userInfo = req.body;
+  const hashedPassword = await bcrypt.hash(userInfo.password, 10);
+  userInfo.password = hashedPassword;
+  const result = await appertmentsCollection.insertOne({
+    ...userInfo,
+    password: hashedPassword,
+    status: "pending",
+  });
+  res.send(result);
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await appertmentsCollection.findOne({ email });
+    if (user) {
+      const isMatch = bcrypt.compareSync(password, user.password);
+      if (isMatch) {
+        console.log(isMatch);
+
+        const token = jwt.sign(
+          { email: user.email },
+          process.env.ACCESS_TOKEN_SECRET,
+          {
+            expiresIn: "365d",
+          }
+        );
+        res.cookie("token", token, cookieOptions).send({
+          success: "true",
+        });
+      } else {
+        res.status(400).send("Password is incorrect");
+      }
+    } else {
+      res.status(400).send("User does not exist");
+    }
+  } catch (error) {
+    console.error("Error logging in user:", error);
+    res.status(500).send("Internal server error");
+  }
+});
+
+app.post("/logout", (req, res) => {
+  res
+    .clearCookie("token", {
+      ...cookieOptions,
+      maxAge: 0,
+    })
+    .send({
+      success: "true",
+    });
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
